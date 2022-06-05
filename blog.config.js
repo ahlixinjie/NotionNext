a6a5cd0fd30e489c627a72b9c490d63c03af0df8
Xinjie Li <xinjie.li@shopee.com>
2022-06-05 19:49:13 +0800

    update footer

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@
 const BLOG = {
   AUTHOR: 'sinko', // 作者
   BIO: '升级打怪中~', // 作者简介
-  LINK: 'https://xinjie.life', // 网站地址
+  LINK: 'https://blog.xinjie.life', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
   process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
